25de132d8eac06be80c8f89d784eeb971cb721ca
jaskim9824 <jaskim9824@gmail.com>
2021-10-20 16:12:51 -0600

    Resolving merge conflicts

--- app/src/main/java/com/example/habitsmasher/ui/dashboard/HabitItemAdapter.java
+++ app/src/main/java/com/example/habitsmasher/ui/dashboard/HabitItemAdapter.java
@@ -4,32 +4,51 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.Button;
 import android.widget.ImageView;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.habitsmasher.Habit;
 import com.example.habitsmasher.HabitList;
 import com.example.habitsmasher.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.firebase.ui.firestore.ObservableSnapshotArray;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Locale;
 
-public class HabitItemAdapter extends RecyclerView.Adapter<HabitItemAdapter.HabitViewHolder> {
+/**
+ * The habit item adapter is the custom adapter for the RecyclerView that holds the habit list
+ */
+public class HabitItemAdapter extends FirestoreRecyclerAdapter<Habit, HabitItemAdapter.HabitViewHolder> {
+
     private static final String DATE_PATTERN = "dd-MM-yyyy";
     private static final Locale LOCALE = Locale.CANADA;
-
-    private final Context _context;
+    private Context _context;
     private static HabitList _habits;
     private static HabitListFragment _habitListFragment;
-    public HabitItemAdapter(Context context, HabitList habits, HabitListFragment fragment) {
-        _context = context;
+    private final FragmentActivity _activity;
+    private final ObservableSnapshotArray<Habit> _snapshots;
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options the firestore entities
+     */
+    public HabitItemAdapter(@NonNull FirestoreRecyclerOptions<Habit> options, FragmentActivity activity,HabitList habits,HabitListFragment fragment) {
+        super(options);
+        _snapshots = options.getSnapshots();
+        _activity = activity;
         _habits = habits;
         _habitListFragment = fragment;
     }
@@ -37,32 +56,63 @@ public class HabitItemAdapter extends RecyclerView.Adapter<HabitItemAdapter.Habi
     @NonNull
     @Override
     public HabitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        _context = parent.getContext();
         View view = LayoutInflater.from(_context).inflate(R.layout.habit_row, parent, false);
         return new HabitViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HabitViewHolder holder, int position) {
-        //Habit currentHabit = _habits.get(position);
-        Habit currentHabit = _habits.getHabitList().get(position);
+    protected void onBindViewHolder(@NonNull HabitViewHolder holder,
+                                    int position,
+                                    @NonNull Habit habit) {
+        // set necessary elements of the habit
+        holder._habitTitle.setText(habit.getTitle());
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_PATTERN, LOCALE);
-
-        holder._habitTitle.setText(currentHabit.getTitle());
 
         setOnClickListenerForHabit(holder, position);
     }
 
+    @Override
+    public void onDataChanged() {
+        // populate local List to match database
+        if (_habits.getHabitList().isEmpty()) {
+            populateHabitList();
+        }
+        super.onDataChanged();
+    }
+
     private void setOnClickListenerForHabit(@NonNull HabitViewHolder holder, int position) {
-        holder._habitRows.setOnClickListener(view -> {
-            // placeholder, just displays a message to indicate the habit has been clicked
-            Toast.makeText(_context, _habits.getHabitList().get(position).getTitle(), Toast.LENGTH_SHORT).show();
-        });
+        holder._habitRows.setOnClickListener(view -> openHabitView(holder, position));
+    }
+
+    /**
+     * This function opens the habit view
+     * @param holder
+     * This holds the values for the selected habit item
+     * @param position
+     * This is the position of the selected habit item
+     */
+    private void openHabitView(HabitViewHolder holder, int position) {
+        Habit currentHabit = _snapshots.get(position);
+        // Create Habit View Fragment with all required parameters passed in
+        HabitViewFragment fragment = HabitViewFragment.newInstance(currentHabit.getReason(), currentHabit.getDate().toString());
+        // Replace the current fragment with the habit view
+        FragmentTransaction transaction = _activity.getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, fragment);
+        transaction.addToBackStack(null);
+        // Load new fragment
+        transaction.commit();
     }
 
     @Override
     public int getItemCount() {
-        return _habits.getHabitList().size();
+        return _snapshots.size();
+    }
+
+    public void populateHabitList() {
+        for (int i = 0; i < _snapshots.size(); i++) {
+            Habit addHabit = _snapshots.get(i);
+            _habits.getHabitList().add(addHabit);
+        }
     }
 
     public static class HabitViewHolder extends RecyclerView.ViewHolder {
@@ -102,3 +152,4 @@ public class HabitItemAdapter extends RecyclerView.Adapter<HabitItemAdapter.Habi
         }
     }
 }
+
