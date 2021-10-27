784f5947d80c9d4bd4067b434aa4f084d4ea4d23
jhmpilz <Jpilz@ualberta.ca>
2021-10-26 18:12:41 -0600

    UI fixes, added date picker

--- app/src/main/java/com/example/habitsmasher/ui/history/AddHabitEventDialog.java
+++ app/src/main/java/com/example/habitsmasher/ui/history/AddHabitEventDialog.java
@@ -1,11 +1,16 @@
 package com.example.habitsmasher.ui.history;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,12 +18,16 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.habitsmasher.R;
 
-public class AddHabitEventDialog extends DialogFragment {
+import java.util.Calendar;
+
+public class AddHabitEventDialog extends DialogFragment implements DatePickerDialog.OnDateSetListener {
     private static final String TAG = "AddHabitEventDialog";
 
     private String _username;
 
     private EditText _eventCommentText;
+    private TextView _eventDateText;
+    private Button _cancelNewEvent;
 
     public AddHabitEventDialog(String username) {
         _username = username;
@@ -29,9 +38,47 @@ public class AddHabitEventDialog extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.add_habit_event_dialog, container, false);
         _eventCommentText = view.findViewById(R.id.habit_event_comment);
+        _eventDateText = view.findViewById(R.id.habit_event_date_selection);
+        _cancelNewEvent = view.findViewById(R.id.cancel_habit_event);
+
+        _eventDateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDatePickerDialog();
+            }
+        });
+
+        _cancelNewEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Cancel");
+                getDialog().dismiss();
+            }
+        });
 
 
         return view;
+    }
+
+    private void openDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                getActivity(),
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        //1 is added to the month we get from the DatePickerDialog
+        // because DatePickerDialog returns values between 0 and 11,
+        // which is not really helpful for users.
+        int correctedMonth = month + 1;
+        String date = day + "/" + correctedMonth + "/" + year;
+        _eventDateText.setText(date);
     }
 
     @Override
