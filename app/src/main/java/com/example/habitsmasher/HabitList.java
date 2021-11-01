4349985a8321e66c75fee2a7e93202ab3a4669a6
kaden-dreger <kkdreger@gmail.com>
2021-11-01 16:41:25 -0600

    Removed unused imports

--- app/src/main/java/com/example/habitsmasher/HabitList.java
+++ app/src/main/java/com/example/habitsmasher/HabitList.java
@@ -7,26 +7,17 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.firebase.ui.firestore.ObservableSnapshotArray;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 
 /**
  * The habit list class is a container for the list of habits.
