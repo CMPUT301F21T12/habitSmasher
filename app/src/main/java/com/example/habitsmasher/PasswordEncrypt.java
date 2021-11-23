bf3863eeeb305fc5050e4393f32b5084577ddd49
JNguyen <jvnguyen@ualberta.ca>
2021-11-22 20:26:07 -0700

    Added Javadoc

--- app/src/main/java/com/example/habitsmasher/PasswordEncrypt.java
+++ app/src/main/java/com/example/habitsmasher/PasswordEncrypt.java
@@ -8,11 +8,16 @@ public class PasswordEncrypt {
 
     private static final int ASCII_TABLE_MAX = 126;
 
+    /**
+     * Caesar Cipher the password to store in the database.
+     * @param password
+     * @return
+     */
     public static String encrypt(String password) {
         int key = 7;
         String newPassword = "";
         for (int i = 0; i < password.length(); i++) {
-            if (password.charAt(i) > Math.abs(ASCII_TABLE_MAX-key)) {
+            if (password.charAt(i) > ASCII_TABLE_MAX-key) {
                 newPassword += (char) (password.charAt(i) - key);
             } else {
                 newPassword += (char) (password.charAt(i) + key);
