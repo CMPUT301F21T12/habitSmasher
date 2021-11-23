e813a85568463729cdbf1fbdb5583b932795ebb5
JNguyen <jvnguyen@ualberta.ca>
2021-11-22 19:10:17 -0700

    Got rid of random characters used before and after password that was encrypted.

--- app/src/main/java/com/example/habitsmasher/PasswordEncrypt.java
+++ app/src/main/java/com/example/habitsmasher/PasswordEncrypt.java
@@ -1,26 +1,21 @@
 package com.example.habitsmasher;
 
+/**
+ * This class encrypts the password the user enters to be stored in the database.
+ * @author Jacob Nguyen
+ */
 public class PasswordEncrypt {
 
-    private static final String BEGINNING_RANDOM_CHARACTERS = "scjOsmSpTHF#BB98w9%*bDSVcdbHUgafFVvbfd*b89FB398^BW9b(b9B";
-    private String ENDING_RANDOM_CHARACTERS = "lB28PG&$d8RHSA98tr3aJENB9T3mntcwVfM6NMrMUcidgD&s9y3rDw*&^3rdS";
-    int _key = 7;
-
-    public String encrypt(String password) {
-        //_key = new Random().nextInt(10);
+    public static String encrypt(String password) {
+        int key = 117;
         String newPassword = "";
-
-        newPassword += BEGINNING_RANDOM_CHARACTERS;
-        for (int i = 0; i<password.length(); i++) {
-            if (password.charAt(i) > 127-_key) {
-                newPassword += (char)(password.charAt(i) - _key);
+        for (int i = 0; i < password.length(); i++) {
+            if (password.charAt(i) > Math.abs(127-key)) {
+                newPassword += (char) (password.charAt(i) - key);
             } else {
-                newPassword += (char)(password.charAt(i) + _key);
+                newPassword += (char) (password.charAt(i) + key);
             }
-
         }
-        newPassword += ENDING_RANDOM_CHARACTERS;
-        newPassword += _key;
         return newPassword;
     }
 }
