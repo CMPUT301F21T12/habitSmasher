8dca5a4459319db5da61f6a05546686f57cbad94
jhmpilz <Jpilz@ualberta.ca>
2021-11-22 18:02:13 -0700

    Added user image to profile page

--- app/src/main/java/com/example/habitsmasher/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/habitsmasher/ui/profile/ProfileFragment.java
@@ -1,11 +1,17 @@
 package com.example.habitsmasher.ui.profile;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,11 +19,17 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.habitsmasher.HabitEvent;
 import com.example.habitsmasher.R;
 import com.example.habitsmasher.User;
 import com.example.habitsmasher.UserDatabaseHelper;
+import com.example.habitsmasher.ui.history.HabitEventItemAdapter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 /**
  * UI class that represents and specifies the behaviour of the user's profile screen
@@ -31,6 +43,9 @@ public class ProfileFragment extends Fragment {
     private ProfileFragment _fragment = this;
     private TextView _numberOfFollowers;
     private TextView _numberOfFollowing;
+    private ImageView _userImageView;
+    private Bitmap _userImage;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -46,6 +61,7 @@ public class ProfileFragment extends Fragment {
         TextView usernameTextView = view.findViewById(R.id.username);
         _numberOfFollowers = view.findViewById(R.id.number_followers);
         _numberOfFollowing = view.findViewById(R.id.number_following);
+        _userImageView = view.findViewById(R.id.profile_picture);
         FloatingActionButton logoutButton = view.findViewById(R.id.logout_button);
 
         // set the UI elements
@@ -57,6 +73,7 @@ public class ProfileFragment extends Fragment {
         userDatabaseHelper.setFollowerCountOfUser();
 
         setClickListenerForLogoutButton(logoutButton);
+        fetchUserImageFromDB(currentUserId);
 
         return view;
     }
@@ -80,5 +97,31 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    private void fetchUserImageFromDB(String userId) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReference();
+
+        StorageReference ref = storageReference.child("images/" + userId + "/" + "userImage");
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+
+        ref.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                _userImage = bm;
+                _userImageView.setImageBitmap(_userImage);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "Failed to get image");
+                // If the image hasn't finished uploading to the db yet, try again
+                fetchUserImageFromDB(userId);
+
+            }
+        });
     }
 }
