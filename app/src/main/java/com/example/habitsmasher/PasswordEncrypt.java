6e6fcd2eb96534bbcecaa316590998199828557d
JNguyen <jvnguyen@ualberta.ca>
2021-11-22 19:13:35 -0700

    Changed encryption key

--- app/src/main/java/com/example/habitsmasher/PasswordEncrypt.java
+++ app/src/main/java/com/example/habitsmasher/PasswordEncrypt.java
@@ -7,7 +7,7 @@ package com.example.habitsmasher;
 public class PasswordEncrypt {
 
     public static String encrypt(String password) {
-        int key = 117;
+        int key = 7;
         String newPassword = "";
         for (int i = 0; i < password.length(); i++) {
             if (password.charAt(i) > Math.abs(127-key)) {
